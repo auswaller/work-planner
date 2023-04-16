b8b7c5a4b68f9b72087e45859825da80edea9e13
Austin W <auswaller@gmail.com>
2023-04-16 10:18:42 -0400

    formatted header HTML, added interval to display current time and date, started code for saving to local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let intervalID;
+let curTimeDate;
+let curSchedule;
+
+
 $(function () {
+  let timeEl = $("#currentDay");
+
+
+  intervalID = setInterval(function(){
+    curTimeDate = dayjs();
+    timeEl.text(curTimeDate.format("dddd, MMMM DD, YYYY - hh:mm A"));
+  }, 1000);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,6 +22,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -18,6 +32,13 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
 });
+
+function saveToLocal (info){
+  localStorage.setItem("schedule", info);
+}
+
+function getFromLocal (){
+  curSchedule = localStorage.getItem("schedule");
+}
